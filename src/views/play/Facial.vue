0bea4a674deae3b71f0989a145c178a0dfd04cba
codewait <644493354@qq.com>
2019-02-18 23:17:55 +0800

    游戏弹窗提交

---
+++ src/views/play/Facial.vue
@@ -0,0 +1,54 @@
+<template>
+  <Dialog
+    :visible.sync="visible"
+    titleImg="/static/game/PrizeTitle2.png"
+    :hasSureButton="true"
+    @sureCallBack="visible = false">
+    <div slot="body" class="facial-mask">
+      <img class="facial-mask-img" src="static/game/Prize2.png" alt="">
+
+      <p class="facial-mask-info">恭喜获得果本面膜一片！请去“我的奖品”页面领取</p>
+    </div>
+  </Dialog>
+</template>
+
+<script>
+import Dialog from '@/components/Dialog'
+
+export default {
+  components: {
+    Dialog
+  },
+  data () {
+    return {
+      visible: false
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.facial-mask {
+  width: 90%;
+  margin: 0 auto;
+  background-color: #fffeee;
+  border: 2px solid #a5500f;
+  border-radius: 5px;
+  position: relative;
+  padding: 0 .5rem .5rem .5rem;
+  
+  .facial-mask-img {
+    width: 70%;
+    padding: 10%;
+    background-image: url(~@/assets/user/PrizeBg.png);
+    background-size: 100% 100%;
+    background-repeat: no-repeat;
+    background-position: center;
+  }
+  .facial-mask-info {
+    font-size: .3rem;
+    font-weight: bold;
+    color: #a5500f;
+  }
+}
+</style>
---
+++ src/views/play/Over.vue
@@ -0,0 +1,52 @@
+<template>
+  <Dialog
+    :visible.sync="visible"
+    titleImg="/static/game/PrizeTitle4.png"
+    :hasSureButton="true"
+    @sureCallBack="visible = false">
+    <div slot="body" class="over-content">
+      <p class="over-content-info">游戏结束！本轮游戏得分</p>
+      <p class="over-content-score">130</p>
+    </div>
+  </Dialog>
+</template>
+
+<script>
+import Dialog from '@/components/Dialog'
+
+export default {
+  components: {
+    Dialog
+  },
+  data () {
+    return {
+      visible: false
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.over-content {
+  width: 90%;
+  margin: 0 auto;
+  background-color: #fffeee;
+  border: 2px solid #d65d18;
+  border-radius: .3rem;
+  position: relative;
+  padding: 0 .5rem .5rem .5rem;
+  
+  .over-content-info {
+    font-size: .4rem;
+    font-weight: bold;
+    color: #d65d18;
+    margin: .4rem 0;
+  }
+
+  .over-content-score {
+    font-size: 1.2rem;
+    font-weight: bold;
+    color: #d65d18;
+  }
+}
+</style>
