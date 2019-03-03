2fc83075f5aebb74378bfaee1143732ff433fe54
huk <372521918@qq.com>
2019-03-03 21:31:49 +0800

    ff

--- src/App.vue
+++ src/App.vue
@@ -47,18 +47,13 @@ export default {
     },
     getCookie(name) {
       if (document.cookie.length > 0) {
-          this.cookie = document.cookie
-          c_start = document.cookie.indexOf(name + "=")//获取字符串的起点
-          if (c_start != -1) {
-              
-              c_start = c_start + name.length + 1//获取值的起点
-              c_end = document.cookie.indexOf(";", c_start)//获取结尾处
-
-              if (c_end == -1) {
-                  c_end = document.cookie.length
-              }
-              //如果是最后一个，结尾就是cookie字符串的结尾
-              return decodeURI(document.cookie.substring(c_start, c_end))//截取字符串返回
+          let cookies = this.cookie = document.cookie
+          if(cookies) {
+            let ca = cookies.split(';')
+            let fined = ca.filter(item => {
+              return item.split('=')[0] == name
+            })
+            return fined[0].split('=')[1]
           }
       }
       return "";
