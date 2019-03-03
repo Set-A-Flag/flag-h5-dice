70f574f8fa3896df5f546922b41762ffd646b6dd
huk <372521918@qq.com>
2019-03-03 21:23:00 +0800

    xie jianl i

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,12 @@
       <img src="@/assets/beijing.png">
     </div>
     <router-view v-if="routerShow"/>
+    cookie: {{cookie}}
+    value: {{value}}
     {{hasId}} ang {{xxx}} xxxxxxxx
   </div>
 </template>
 <script>
-import { getCookie } from '@/tools/commonTool.js'
 import { wechatAuth } from '@/api'
 
 export default {
@@ -18,7 +19,8 @@ export default {
   data() {
     return {
       hasId: '',
-      xxx: ''
+      xxx: '',
+      cookie: ''
     }
   },
   methods: {
@@ -26,7 +28,8 @@ export default {
     //   return process.env.NODE_ENV === 'production'
     // }
     checkLogined() {
-      let openId = getCookie('openId')
+      let openId = this.getCookie('openId')
+      this.value = openId
       let state = 'http://www.13idea.com'
 
       if(!openId) {
@@ -41,6 +44,24 @@ export default {
         })
         // this.logined = true
       }
+    },
+    getCookie(name) {
+      if (document.cookie.length > 0) {
+          this.cookie = document.cookie
+          c_start = document.cookie.indexOf(name + "=")//获取字符串的起点
+          if (c_start != -1) {
+              
+              c_start = c_start + name.length + 1//获取值的起点
+              c_end = document.cookie.indexOf(";", c_start)//获取结尾处
+
+              if (c_end == -1) {
+                  c_end = document.cookie.length
+              }
+              //如果是最后一个，结尾就是cookie字符串的结尾
+              return decodeURI(document.cookie.substring(c_start, c_end))//截取字符串返回
+          }
+      }
+      return "";
     }
   }
 }
